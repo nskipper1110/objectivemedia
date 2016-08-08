e684e681b52a065a91ed81f8beef8a8001999b9f
nathanskipper <nathanskipper@192.168.9.33>
2016-08-08 09:19:58 -0500

    Merging

--- ffmpeg/doc/print_options.d
+++ ffmpeg/doc/print_options.d
@@ -1,15 +1,11 @@
 doc/print_options.o: doc/print_options.c libavformat/avformat.h \
- libavcodec/avcodec.h libavutil/samplefmt.h libavutil/avutil.h \
- libavutil/common.h libavutil/attributes.h libavutil/version.h \
- libavutil/macros.h libavutil/avconfig.h libavutil/mem.h \
- libavutil/error.h libavutil/mathematics.h libavutil/rational.h \
- libavutil/intfloat.h libavutil/log.h libavutil/pixfmt.h \
- libavutil/old_pix_fmts.h libavutil/attributes.h libavutil/avutil.h \
- libavutil/buffer.h libavutil/cpu.h libavutil/channel_layout.h \
- libavutil/dict.h libavutil/frame.h libavutil/buffer.h libavutil/dict.h \
- libavutil/samplefmt.h libavutil/log.h libavutil/pixfmt.h \
- libavutil/rational.h libavcodec/version.h libavutil/version.h \
- libavutil/mem.h libavcodec/old_codec_ids.h libavutil/common.h \
- libavformat/avio.h libavformat/version.h libavformat/options_table.h \
- libavutil/opt.h libavformat/avformat.h libavcodec/options_table.h \
- libavcodec/avcodec.h
+  libavcodec/avcodec.h libavutil/samplefmt.h libavutil/avutil.h \
+  libavutil/common.h libavutil/attributes.h libavutil/version.h \
+  libavutil/macros.h libavutil/avconfig.h libavutil/mem.h \
+  libavutil/error.h libavutil/mathematics.h libavutil/rational.h \
+  libavutil/intfloat.h libavutil/log.h libavutil/pixfmt.h \
+  libavutil/old_pix_fmts.h libavutil/buffer.h libavutil/cpu.h \
+  libavutil/channel_layout.h libavutil/dict.h libavutil/frame.h \
+  libavcodec/version.h libavcodec/old_codec_ids.h libavformat/avio.h \
+  libavformat/version.h libavformat/options_table.h libavutil/opt.h \
+  libavcodec/options_table.h
--- ffmpeg/libavutil/ffversion.h
+++ ffmpeg/libavutil/ffversion.h
@@ -1,8 +1,4 @@
 #ifndef AVUTIL_FFVERSION_H
 #define AVUTIL_FFVERSION_H
-<<<<<<< HEAD
-#define FFMPEG_VERSION "git-2016-08-04-0c5dee5"
-=======
-#define FFMPEG_VERSION "git-2016-08-01-4424642"
->>>>>>> b3c912f41532d7fdc20c4e2469fba06c5fedbb36
+#define FFMPEG_VERSION "git-2016-08-05-21cdd63"
 #endif /* AVUTIL_FFVERSION_H */
